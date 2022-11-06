8485b50c4c2bea465a98f21213213216fd190ed3
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-11-06 06:33:10 +0530

    feat: added test for badge custom sizes :white_check_mark: :test_tube:

--- src/lib/components/Badge/Badge.test.tsx
+++ src/lib/components/Badge/Badge.test.tsx
@@ -47,5 +47,38 @@ describe('Components / Badge', () => {
         'bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800 group-hover:bg-blue-200 dark:group-hover:bg-blue-300',
       );
     });
+
+    it('should custom sizes', () => {
+      const theme = {
+        badge: {
+          icon: {
+            off: 'rounded-lg p-1',
+            on: 'rounded-full p-5',
+            size: {
+              xxl: 'w-6 h-6',
+            },
+          },
+          size: {
+            xxl: 'text-2xl',
+          },
+        },
+      };
+
+      render(
+        <ReuseUI theme={{ theme }}>
+          <Badge size='xxl'>A badge</Badge>
+          <Badge icon={HiCheck} size='xxl' />
+        </ReuseUI>,
+      );
+
+      const badges = screen.getAllByTestId('ReuseUI-badge');
+      const regularBadge = badges[0];
+      const emptyBadge = badges[1];
+
+      expect(regularBadge).toHaveClass('text-2xl');
+      expect(regularBadge).toHaveClass('rounded-lg p-1');
+      expect(emptyBadge).toHaveClass('rounded-full p-5');
+      expect(icon()).toHaveClass('w-6 h-6');
+    });
   });
 });
