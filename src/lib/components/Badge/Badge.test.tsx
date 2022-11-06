65aa4ba77a1b1afa4639705f6a248c5abb872630
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-11-06 06:18:06 +0530

    feat: added badge custom theme test :white_check_mark: :test_tube:

--- src/lib/components/Badge/Badge.test.tsx
+++ src/lib/components/Badge/Badge.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { HiCheck } from 'react-icons/hi';
 import { describe, expect, it } from 'vitest';
+import { ReuseUI } from '../ReuseUI';
 import { Badge } from './Badge';
 
-const badge = () => screen.getByTestId('flowbite-badge');
+const badge = () => screen.getByTestId('ReuseUI-badge');
 
-const icon = () => screen.getByTestId('flowbite-badge-icon');
+const icon = () => screen.getByTestId('ReuseUI-badge-icon');
 
 const link = () => screen.getByRole('link');
 
@@ -20,6 +21,31 @@ describe('Components / Badge', () => {
 
       expect(link()).toBeInTheDocument();
       expect(link()).toHaveAttribute('href', '/');
+    });
+  });
+
+  describe('Theme', () => {
+    it('should use custom colors', () => {
+      const theme = {
+        badge: {
+          color: {
+            primary:
+              'bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800 group-hover:bg-blue-200 dark:group-hover:bg-blue-300',
+          },
+        },
+      };
+
+      render(
+        <ReuseUI theme={{ theme }}>
+          <Badge color='primary' href='/' icon={HiCheck}>
+            A badge
+          </Badge>
+        </ReuseUI>,
+      );
+
+      expect(badge()).toHaveClass(
+        'bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800 group-hover:bg-blue-200 dark:group-hover:bg-blue-300',
+      );
     });
   });
 });
