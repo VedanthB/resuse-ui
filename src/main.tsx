710584429f942fe7b001e65f6db26be8f491ae80
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2023-04-03 23:41:57 +0530

    refactor: tooltip :recycle:

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,10 @@ if (container) {
 
   const theme = {
     sidebar: {
-      base: 'h-full bg-inherit',
-      inner: 'h-full overflow-y-auto overflow-x-hidden rounded bg-inherit py-4 px-3',
+      root: {
+        base: 'h-full bg-inherit',
+        inner: 'h-full overflow-y-auto overflow-x-hidden rounded bg-inherit py-4 px-3',
+      },
     },
   };
 
