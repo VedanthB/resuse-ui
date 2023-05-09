60c660fac83b0e79c9a16ba6fe6a81c24f5993ac
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2023-05-09 12:50:01 +0530

    feat: added card example with dropdown :sparkles:

--- src/docs/pages/CardPage.tsx
+++ src/docs/pages/CardPage.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-import { Button, Card } from '../../lib';
+import { Button, Card, Dropdown } from '../../lib';
 import { CodeExample, DemoPage, PageContent } from './DemoPage';
 
 const CardPage: FC = () => {
@@ -116,71 +116,71 @@ const CardPage: FC = () => {
       ),
       codeClassName: 'dark:!bg-gray-900',
     },
-    // {
-    //   title: 'User profile card',
-    //   content: '',
-    //   code: (
-    //     <div className='max-w-sm'>
-    //       <Card>
-    //         <div className='flex justify-end px-4 pt-4'>
-    //           {/* TODO: add dropdown comp and then add this */}
-    //           <Dropdown inline label=''>
-    //             <Dropdown.Item>
-    //               <a
-    //                 href='#'
-    //                 className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white'
-    //               >
-    //                 Edit
-    //               </a>
-    //             </Dropdown.Item>
-    //             <Dropdown.Item>
-    //               <a
-    //                 href='#'
-    //                 className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white'
-    //               >
-    //                 Export Data
-    //               </a>
-    //             </Dropdown.Item>
-    //             <Dropdown.Item>
-    //               <a
-    //                 href='#'
-    //                 className='block py-2 px-4 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white'
-    //               >
-    //                 Delete
-    //               </a>
-    //             </Dropdown.Item>
-    //           </Dropdown>
-    //         </div>
-    //         <div className='flex flex-col items-center pb-10'>
-    //           <img
-    //             className='mb-3 h-24 w-24 rounded-full shadow-lg'
-    //             src='https://flowbite.com/docs/images/people/profile-picture-3.jpg'
-    //             alt='Bonnie image'
-    //           />
-    //           <h5 className='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
-    //             Bonnie Green
-    //           </h5>
-    //           <span className='text-sm text-gray-500 dark:text-gray-400'>Visual Designer</span>
-    //           <div className='mt-4 flex space-x-3 lg:mt-6'>
-    //             <a
-    //               href='#'
-    //               className='inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-    //             >
-    //               Add friend
-    //             </a>
-    //             <a
-    //               href='#'
-    //               className='inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700'
-    //             >
-    //               Message
-    //             </a>
-    //           </div>
-    //         </div>
-    //       </Card>
-    //     </div>
-    //   ),
-    //   codeClassName: 'dark:!bg-gray-900',
-    // },
+    {
+      title: 'User profile card',
+      content: '',
+      code: (
+        <div className='max-w-sm'>
+          <Card>
+            <div className='flex justify-end px-4 pt-4'>
+              {/* TODO: add dropdown comp and then add this */}
+              <Dropdown inline label=''>
+                <Dropdown.Item>
+                  <a
+                    href='#'
+                    className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white'
+                  >
+                    Edit
+                  </a>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <a
+                    href='#'
+                    className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white'
+                  >
+                    Export Data
+                  </a>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <a
+                    href='#'
+                    className='block py-2 px-4 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white'
+                  >
+                    Delete
+                  </a>
+                </Dropdown.Item>
+              </Dropdown>
+            </div>
+            <div className='flex flex-col items-center pb-10'>
+              <img
+                className='mb-3 h-24 w-24 rounded-full shadow-lg'
+                src='https://flowbite.com/docs/images/people/profile-picture-3.jpg'
+                alt='Bonnie image'
+              />
+              <h5 className='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
+                Bonnie Green
+              </h5>
+              <span className='text-sm text-gray-500 dark:text-gray-400'>Visual Designer</span>
+              <div className='mt-4 flex space-x-3 lg:mt-6'>
+                <a
+                  href='#'
+                  className='inline-flex items-center rounded-lg bg-blue-700 py-2 px-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                >
+                  Add friend
+                </a>
+                <a
+                  href='#'
+                  className='inline-flex items-center rounded-lg border border-gray-300 bg-white py-2 px-4 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700'
+                >
+                  Message
+                </a>
+              </div>
+            </div>
+          </Card>
+        </div>
+      ),
+      codeClassName: 'dark:!bg-gray-900',
+    },
     // {
     //   title: 'Card with form inputs',
     //   content: '',
@@ -193,7 +193,7 @@ const CardPage: FC = () => {
     //               {/* TODO: add label, text input , checkbox and button comp and then add this */}
     //               <Label htmlFor='email1' value='Your email' />
     //             </div>
-    //             <TextInput id='email1' type='email' placeholder='name@flowbite.com' required />
+    //             <TextInput id='email1' type='email' placeholder='name@ReuseUI.com' required />
     //           </div>
     //           <div>
     //             <div className='mb-2 block'>
@@ -294,7 +294,7 @@ const CardPage: FC = () => {
             Work fast from anywhere
           </h5>
           <p className='mb-5 text-base text-gray-500 dark:text-gray-400 sm:text-lg'>
-            Stay up to date and move work forward with Flowbite on iOS & Android. Download the app
+            Stay up to date and move work forward with ReuseUI on iOS & Android. Download the app
             today.
           </p>
           <div className='items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4'>
