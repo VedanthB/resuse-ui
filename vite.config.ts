003c3466c257444a254a5b556d39c1de0641e3bd
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2023-09-11 22:39:54 +0530

    feat: publish to npm first time :sparkles:

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 import { defineConfig } from 'vite';
-
+import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), dts()],
   test: {
     coverage: {
       reporter: ['html', 'json', 'text'],
@@ -17,5 +18,23 @@ export default defineConfig({
     exclude: ['lib', 'node_modules', 'docs'],
     globals: true,
     setupFiles: 'src/setup-tests.ts',
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'reuse-ui',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
   },
 });
